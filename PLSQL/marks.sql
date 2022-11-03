3b96c2dab96d21a26e07594676bd883853a8ddab
Ashmin Jayson <ashminjayson10@gmail.com>
2022-11-03 15:31:29 +0530

    Add files via upload

--- PLSQL/marks.sql
+++ PLSQL/marks.sql
@@ -1,11 +1,21 @@
 set serveroutput on
+accept x number prompt 'Input the Roll Number of the student';
 declare 
+rno number(10);
+mk1 number(10);
+mk2 number(10);
+mk3 number(10);
 avgm number(10);
 today varchar(20);
 weekday varchar(20);
 begin
-select avg(marks) into avgm from students;
+rno:=&x;
+select mark1 into mk1 from students where rollno = rno;
+select mark2 into mk2 from students where rollno = rno;
+select mark2 into mk3 from students where rollno = rno;
 
+avgm:=mk1+mk2+mk3;
+avgm:=avgm/3;
 dbms_output.put_line('Average Marks : ' || avgm);
 if avgm < 40 then
 	dbms_output.put_line('Need Improvement');
